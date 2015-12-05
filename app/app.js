f25ada15e99c9d1a7bbfa9ae29f817af7f503be9
bokapaatha <boka.paatha@gmail.com>
2015-12-05 23:27:47 +0530

    Refactory controller logic in its own js.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
 require('angular')
+// This is neat. Export from the other js. Catch it in a variable here.
+// It is starting to look a lot like C or something like that.
+var MainController = require('./controllers/MainController')
 
 var app = angular.module('app', [])
 
-app.controller('MainController', function($scope) {
-    $scope.message = 'Hello world from Angular. And this message can change on the fly. Once more. Yahoooooo.'
-})
+//This is the old way. 
+//app.controller('MainController', function($scope) {
+//    $scope.message = 'Hello world from Angular. And this message can change on the fly. Once more. Yahoooooo.'
+//})
+
+// This is the new way. No functionality coded here. 
+app.controller('MainController', ['$scope', MainController])
---
+++ app/controllers/MainController.js
@@ -0,0 +1,6 @@
+//Write the functionality and send it back 
+//Really lean packaging. Only a function. That too without a name. 
+
+module.exports = function($scope) {
+    $scope.message = 'There is some killer logic here.'
+}
