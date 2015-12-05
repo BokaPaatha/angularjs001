ad58e1c6c8696f5bdf964b6040f51be7db58ceaa
bokapaatha <boka.paatha@gmail.com>
2015-12-05 23:01:23 +0530

    Wired up browserify and angularjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp')
 var sass = require('gulp-ruby-sass')
 var connect = require('gulp-connect')
+// requires browserify and vinyl-source-stream
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
 
 // Create a basic server. Serve it from public folder on port 4000.
 gulp.task('connect', function() {
@@ -8,4 +11,15 @@ gulp.task('connect', function() {
 		root : 'public',
 		port : 4000
 	})
+})
+
+// Take the Angular file and make it usable. 
+gulp.task('browserify', function() {
+    // Grabs the app.js file
+    return browserify('./app/app.js')
+        // bundles it and creates a file called main.js
+        .bundle()
+        .pipe(source('main.js'))
+        // saves it the public/js/ directory
+        .pipe(gulp.dest('./public/js/'));
 })
